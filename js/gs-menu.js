5eabc879f7cd0c973434e1e1930ec512b2deb7e5
rohanpatel4846 <97083899+rohanpatel4846@users.noreply.github.com>
2022-05-31 17:29:38 +0530

    Changing Active Class on click

--- js/gs-menu.js
+++ js/gs-menu.js
@@ -14,3 +14,16 @@ function gs_closeMenu(ele){
     let mobileMenuEle = ele.parentElement;
     mobileMenuEle.classList.remove("gs-open");
 }
+
+window.onload = function() {
+    let menuLinks = document.querySelectorAll('.gs-menu-link-with-active');
+
+    menuLinks.forEach(menuLink => {
+        menuLink.addEventListener('click', function handleClick(event) {
+            let oldActive = document.querySelector('.gs-menu-link-with-active.gs-menu-active');
+            oldActive.classList.remove("gs-menu-active");
+
+            menuLink.classList.add("gs-menu-active");
+        });
+    });
+};
