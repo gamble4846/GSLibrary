f0a0b35ce22c4b5b39917ebf87550afd2e3ab721
Apurva20997 <96464493+Apurva20997@users.noreply.github.com>
2022-06-10 15:38:34 +0530

    testing

--- components/scroll-food/js/gs-scroll-food.js
+++ components/scroll-food/js/gs-scroll-food.js
@@ -103,3 +103,52 @@ function updatePage(){
         }
     }
 }
+
+
+//------Swiping------------
+container.addEventListener('touchstart', handleTouchStart, false);        
+container.addEventListener('touchmove', handleTouchMove, false);
+
+var xDown = null;                                                        
+var yDown = null;
+
+function getTouches(evt) {
+  return evt.touches ||             // browser API
+         evt.originalEvent.touches; // jQuery
+}                                                     
+                                                                         
+function handleTouchStart(evt) {
+    const firstTouch = getTouches(evt)[0];                                      
+    xDown = firstTouch.clientX;                                      
+    yDown = firstTouch.clientY;                                      
+};                                                
+                                                                         
+function handleTouchMove(evt) {
+    if ( ! xDown || ! yDown ) {
+        return;
+    }
+
+    var xUp = evt.touches[0].clientX;                                    
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+                                                                         
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+        if ( xDiff > 0 ) {
+            totalScroll += 100;
+        } else {
+            totalScroll -=100;
+        }       
+        updatePage();                
+    } else {
+        if ( yDiff > 0 ) {
+            /* down swipe */ 
+        } else { 
+            /* up swipe */
+        }                                                                 
+    }
+    /* reset values */
+    xDown = null;
+    yDown = null;                                
+};
