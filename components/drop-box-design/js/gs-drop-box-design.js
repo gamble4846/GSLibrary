1fc41f63f7dcc8639a9be2a381bc8b133d63d813
Rohan Patel <96464493+Apurva20997@users.noreply.github.com>
2022-07-28 18:31:22 +0530

    Right Click 50 % Done

--- components/drop-box-design/js/gs-drop-box-design.js
+++ components/drop-box-design/js/gs-drop-box-design.js
@@ -39,3 +39,56 @@ function toggleLeftMenu(){
     var leftmenu = document.getElementById("gs-dropbox-left");
     leftmenu.classList.toggle("open");
 }
+
+function openFile(){
+    var fileModel = document.getElementById("gs-dropbox-file-model");
+    fileModel.classList.add("open");
+}
+
+function closeFile(){
+    var fileModel = document.getElementById("gs-dropbox-file-model");
+    fileModel.classList.remove("open");
+}
+
+function showRightClickMenu(event){
+    var rightClickMenu = document.getElementById("gs-drop-box-right-click-menu");
+
+    
+    var _docHeight = (document.height !== undefined) ? document.height : document.body.offsetHeight;
+    var _docWidth = (document.width !== undefined) ? document.width : document.body.offsetWidth;
+
+    rightClickMenu.classList.add("open");
+    var rightClickMenuWidth = 200;
+    rightClickMenu.style.top = event.clientY + "px";
+    
+
+    if((_docWidth - event.clientX) < (rightClickMenuWidth + 20)){
+        rightClickMenu.style.right = _docWidth - event.clientX + "px";
+        rightClickMenu.style.left = "auto";
+        console.log(rightClickMenu.style.right);
+    }
+    else{
+        rightClickMenu.style.right = "auto";
+        rightClickMenu.style.left = event.clientX + "px";
+    }
+
+    rightClickMenu.style.maxHeight = (_docHeight - event.clientY - 20) + "px";
+}
+
+function fullDocumentClick(event){
+    var rightClickMenu = document.getElementById("gs-drop-box-right-click-menu");
+    if(!rightClickMenu.contains(event.target)){
+        rightClickMenu.classList.remove("open");
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    var rightClickElements = document.getElementsByClassName("allowRightClick");
+    console.log(rightClickElements);
+    for (let index = 0; index < rightClickElements.length; index++) {
+        const rightClickElement = rightClickElements[index];
+        rightClickElement.addEventListener("contextmenu", showRightClickMenu);
+    }
+
+    document.addEventListener("click", fullDocumentClick);
+});
